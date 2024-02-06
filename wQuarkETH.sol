e0f70aff900e4d4e78f3d5a1bfdbb65cce68750d
jimango <jonathan.hol@gmail.com>
2024-02-06 12:51:46 +0000

    Add files via upload

---
+++ wQuarkETH.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@cryptolink/contracts/message/MessageClient.sol";
+import "./Libraries.sol";
+
+
+contract wQuarkETH is MessageClient, Context, Ownable, Pausable {
+
+    address private _quarkTokenAddress;
+    IERC20 private _quarkToken;
+
+    constructor(
+        address quarkTokenAddress,
+        uint256 amountToDeposit
+    ) {
+        require(quarkTokenAddress != address(0), "Cannot be zero address!");
+        require(amountToDeposit > 0, "Amount must be greater than zero!");
+        _quarkTokenAddress = quarkTokenAddress;
+        _quarkToken = IERC20(_quarkTokenAddress);
+        SafeERC20.safeTransferFrom(_quarkToken, msg.sender, address(this), amountToDeposit);
+    }
+
+    function bridge(
+        uint _destChainId,
+        address _recipient,
+        uint _amount
+    ) external onlyActiveChain(_destChainId) whenNotPaused {
+        SafeERC20.safeTransferFrom(_quarkToken, msg.sender, address(this), _amount);
+        _sendMessage(_destChainId, abi.encode(_recipient, _amount));
+    }
+
+    function messageProcess(
+        uint, 
+        uint _sourceChainId, 
+        address _sender, 
+        address, uint, 
+        bytes calldata _data
+    ) external override  onlySelf(_sender, _sourceChainId) whenNotPaused {
+        (address _recipient, uint _amount) = abi.decode(_data, (address, uint));
+        SafeERC20.safeTransferFrom(_quarkToken, address(this), _recipient, _amount);
+    }
+
+    function emergencyRecover() public onlyOwner {
+        uint256 contractTokenBalance = _quarkToken.balanceOf(address(this));
+        SafeERC20.safeTransfer(_quarkToken, msg.sender, contractTokenBalance);
+        pause();
+    }
+
+    function recoverETH() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function pause() public onlyOwner {
+        super._pause();
+    }
+
+    function unpause() public onlyOwner {
+        super._unpause();
+    }
+
+}
